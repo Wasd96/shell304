fa6635eb21a0993487ec167f9d5c77027fe7480e
Alexander Volkov <wasd3680@yandex.ru>
2017-03-27 21:26:11 +0300

    Final version

--- lab.c
+++ lab.c
@@ -4,54 +4,49 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-void exec(char *const argv[], char *str, char *buff)
-{
-	strcpy(str, "/bin/");
-	strcat(str, buff);
-	execv(str, argv);
-	exit(0);
-}
-
 int main(void)
 {
 	char str[128];
 
-	strcpy(str, "Welcome to Shell304!\nI'll perform all you wishes...\n");
+	strcpy(str, "Welcome to Shell304!\nTell me what do you want :)\n");
 	write(1, str, strlen(str));
 	while (1) {
 		int nread = 0;
 		char buff[128];
-		char args[20][20];
+		char *args[20];
 		char *pargs;
+		char *saveptr1;
 		pid_t pid;
 		int i = 0;
 
 		write(1, ">", 2);
 		nread = read(0, buff, 128);
 		if (nread == -1) {
-			strcpy(str, "Some error in \'read\' occures :(");
+			strcpy(str, "Some error in \'read\' occures :(\n");
 			write(2, str, strlen(str));
 			exit(1);
 		}
-		buff[nread-1] = 0;
-		pargs = strtok(buff, " ");
+		if (nread > 0)
+			buff[nread-1] = 0;
+		pargs = strtok_r(buff, " ", &saveptr1);
 		while (pargs != NULL) {
-			pargs = strtok(NULL, " ");
-			args[i][0] = pargs;
-			//write(1, args[i], strlen(args[i]));
-			write(1, pargs, strlen(pargs));
+			args[i] = pargs;
 			i++;
+			pargs = strtok_r(NULL, " ", &saveptr1);
 		}
-		args[i][0] = 0;
+		args[i] = 0;
 		pid = fork();
 		if (pid == -1) {
-			strcpy(str, "Some error in \'fork\' occures :(");
+			strcpy(str, "Some error in \'fork\' occures :(\n");
 			write(2, str, strlen(str));
 			exit(1);
 		} else if (pid == 0) {
-			write(1, buff, strlen(buff));
-			write(1, str, strlen(str));
-			exec(args, str, buff);
+			i = execvp(buff, args);
+			if (i == -1) {
+				strcpy(str, "Cant exec this! }:[\n");
+				write(2, str, strlen(str));
+			}
+			exit(0);
 		} else {
 			wait(NULL);
 		}
